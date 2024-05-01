096913ddb7249a14881e59e2110c5fd11e12542b
VictorIdowu <idsonvictor@gmail.com>
2024-05-01 16:21:26 +0100

    tabs

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QMainWindow,QWidget,QApplication,QGroupBox,QStyleFactory,QLabel,QVBoxLayout,QHBoxLayout,QPushButton
+from PyQt6.QtWidgets import QMainWindow,QWidget,QApplication,QGroupBox,QStyleFactory,QLabel,QVBoxLayout,QTabWidget,QHBoxLayout,QPushButton
 import sys
 
 
@@ -10,33 +10,36 @@ class Window(QWidget):
 
   def initUI(self):
       self.setGeometry(0,0,700,500)
-      # Step 1 - Creating a container
-      box = QGroupBox()
-      box2 = QGroupBox()
+      # Creating a tab widget
+      tab_widget = QTabWidget()
 
-      # Step 2 - Creating widgets to be added to the container
+      # Create tabs
+      tab1 = QWidget()
+      tab2 = QWidget()
+
+      # Add tabs to the tab widget
+      tab_widget.addTab(tab1, "Tab 1")
+      tab_widget.addTab(tab2, "Tab 2")
+      
+      # Create widgets to be added to the tabs
       btn1 = QPushButton("Button 1")
       btn2 = QPushButton("Button 2")
-      btn3 = QPushButton("Button 3")
-      btn4 = QPushButton("Button 4")
 
-      # Step 3 - Create a layout and add the above widgets
-      layout = QVBoxLayout()
-      layout.addWidget(btn1)
-      layout.addWidget(btn2)
+      # Creating layout for the 2 tabs
+      layout1 = QVBoxLayout()
+      layout1.addWidget(btn1)
+      layout2 = QVBoxLayout()
+      layout2.addWidget(btn2)
 
-      layout2 = QHBoxLayout()
-      layout2.addWidget(btn3)
-      layout2.addWidget(btn4)
+      # Setting tabs layouts to layout 1&2
+      tab1.setLayout(layout1)
+      tab2.setLayout(layout2)
+      
 
-      # Step 4 - Add the above layout to container
-      box.setLayout(layout)
-      box2.setLayout(layout2)
-
-      # Step 5 - Set the main window's layout to the container
-      main_layout = QVBoxLayout(self)
-      main_layout.addWidget(box)
-      main_layout.addWidget(box2)
+      # Add the above tab widget to the main window
+      layout = QVBoxLayout(self)
+      layout.addWidget(tab_widget)
+      
       
 
 app = QApplication(sys.argv)
