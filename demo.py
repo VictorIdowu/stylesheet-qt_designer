35f99c58d7b9066f227d7db5f82226a0dc370678
VictorIdowu <idsonvictor@gmail.com>
2024-05-01 16:06:27 +0100

    containers

--- demo.py
+++ demo.py
@@ -1,25 +1,7 @@
-from PyQt6.QtWidgets import QMainWindow,QWidget,QApplication,QStyleFactory,QLabel,QVBoxLayout,QPushButton
+from PyQt6.QtWidgets import QMainWindow,QWidget,QApplication,QGroupBox,QStyleFactory,QLabel,QVBoxLayout,QHBoxLayout,QPushButton
 import sys
 
-stylesheet = """
-                QPushButton#My_button{
-                background-color: gray;
-                padding:5px;
-                border-radius: 10px;
-                color:white;
-                }
-                QPushButton#My_button:hover{
-                background-color: red;
-                }
-                QPushButton#My_button:pressed{
-                background-color: blue;
-                }
-                QLabel#my_label{
-                background-color:blue;
-                color:white;
-                margin:100px;
-                }
-                """
+
 
 class Window(QWidget):
   def __init__(self):
@@ -28,21 +10,37 @@ class Window(QWidget):
 
   def initUI(self):
       self.setGeometry(0,0,700,500)
-      label = QLabel("<h1>Das ist ein label</h1>", self)
-      label.resize(200,50)
-      label.setObjectName("my_label")
-      
-      button = QPushButton("Click me", self)
-      button.setObjectName("My_button")
+      # Step 1 - Creating a container
+      box = QGroupBox()
+      box2 = QGroupBox()
 
+      # Step 2 - Creating widgets to be added to the container
+      btn1 = QPushButton("Button 1")
+      btn2 = QPushButton("Button 2")
+      btn3 = QPushButton("Button 3")
+      btn4 = QPushButton("Button 4")
+
+      # Step 3 - Create a layout and add the above widgets
       layout = QVBoxLayout()
-      layout.addWidget(label)
-      layout.addWidget(button)
-      self.setLayout(layout)
+      layout.addWidget(btn1)
+      layout.addWidget(btn2)
+
+      layout2 = QHBoxLayout()
+      layout2.addWidget(btn3)
+      layout2.addWidget(btn4)
+
+      # Step 4 - Add the above layout to container
+      box.setLayout(layout)
+      box2.setLayout(layout2)
+
+      # Step 5 - Set the main window's layout to the container
+      main_layout = QVBoxLayout(self)
+      main_layout.addWidget(box)
+      main_layout.addWidget(box2)
+      
 
 app = QApplication(sys.argv)
-app.setStyle("Windows")
-app.setStyleSheet(stylesheet)
+app.setStyle("Fusion")
 window = Window()
 window.show()
 app.exec()
